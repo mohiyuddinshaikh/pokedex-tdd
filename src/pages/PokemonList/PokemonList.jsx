7810e066b4040c66e9f66ff936a77f5517399605
Mohiyuddin Shaikh <mohiyuddin.shaikh46@gmail.com>
2026-04-05 18:27:41 +0530

    refactor: use axios with async/await for fetching pokemon data

--- src/pages/PokemonList/PokemonList.jsx
+++ src/pages/PokemonList/PokemonList.jsx
@@ -1,3 +1,27 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
 export default function PokemonList() {
-  return <div>Loading...</div>;
+  const [pokemon, setPokemon] = useState([]);
+
+  async function fetchPokemon() {
+    const response = await axios.get("https://pokeapi.co/api/v2/pokemon");
+    setPokemon(response?.data?.results);
+  }
+
+  useEffect(() => {
+    fetchPokemon();
+  }, []);
+
+  if (pokemon.length === 0) {
+    return <p>Loading...</p>;
+  }
+
+  return (
+    <ul>
+      {pokemon.map((p) => (
+        <li key={p.name}>{p.name}</li>
+      ))}
+    </ul>
+  );
 }
