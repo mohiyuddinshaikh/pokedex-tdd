bd0fc28b1a9bb5b1c92e9a578236a721cab79a9b
Mohiyuddin Shaikh <mohiyuddin.shaikh46@gmail.com>
2026-04-05 18:20:36 +0530

    test: update test to mock axios for pokemon fetch

--- src/pages/PokemonList/PokemonList.test.jsx
+++ src/pages/PokemonList/PokemonList.test.jsx
@@ -1,22 +1,24 @@
 import { render, screen } from "@testing-library/react";
 import { expect, test, vi } from "vitest";
 import PokemonList from "./PokemonList";
+import axios from "axios";
 
 test("shows loading initially", () => {
   render(<PokemonList />);
   expect(screen.getByText(/loading/i)).toBeInTheDocument();
 });
 
+vi.mock("axios");
+
 test("renders pokemon list after fetch", async () => {
-  vi.spyOn(globalThis, "fetch").mockResolvedValue({
-    json: async () => ({
+  axios.get.mockResolvedValue({
+    data: {
       results: [{ name: "pikachu" }, { name: "bulbasaur" }],
-    }),
+    },
   });
 
   render(<PokemonList />);
 
   const items = await screen.findAllByRole("listitem");
-
   expect(items.length).toBe(2);
 });
