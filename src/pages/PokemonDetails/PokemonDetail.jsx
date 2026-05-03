ce7ce05ac0f9b49af4218636fedce44d71e200b4
Mohiyuddin Shaikh <mohiyuddin.shaikh46@gmail.com>
2026-05-03 10:02:03 +0530

    feat: added audio descrption

--- src/pages/PokemonDetails/PokemonDetail.jsx
+++ src/pages/PokemonDetails/PokemonDetail.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import axios from "axios";
 import "./PokemonDetail.css";
+import Speaker from "../../assets/speaker.svg";
 
 export default function PokemonDetail() {
   const { name } = useParams();
@@ -23,15 +24,40 @@ export default function PokemonDetail() {
     fetchPokemon();
   }, [name]);
 
+  const description = (() => {
+    if (!pokemon) return "";
+
+    const abilities =
+      pokemon.abilities?.map((a) => a.ability.name).join(", ") || "unknown";
+
+    const types =
+      pokemon.types?.map((t) => t.type.name).join(", ") || "unknown";
+
+    return `This is ${pokemon.name}. Its height is ${pokemon.height / 10}metre, weight is ${pokemon.weight / 10}kg. Abilities are ${abilities}. Types are ${types}.`;
+  })();
+
+  function speakDescription() {
+    if (!description) return;
+
+    const utterance = new SpeechSynthesisUtterance(description);
+    speechSynthesis.cancel(); // stop any ongoing speech
+    speechSynthesis.speak(utterance);
+  }
+
   if (error) return <p>Something went wrong</p>;
   if (!pokemon) return <p>Loading...</p>;
 
   return (
     <div className="pokemon-detail-container">
-      {/* Back Button */}
-      <button className="pokemon-back-button" onClick={() => navigate(-1)}>
-        Back
-      </button>
+      <div className="utility-container">
+        {/* Back Button */}
+        <button className="pokemon-back-button" onClick={() => navigate(-1)}>
+          Back
+        </button>
+        <button className="icon-button" onClick={() => speakDescription()}>
+          <img src={Speaker} height={35} width={35} />
+        </button>
+      </div>
 
       {/* Title */}
       <h1 className="pokemon-detail-title">{pokemon.name}</h1>
